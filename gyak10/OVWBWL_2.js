94db4107ead6ead25c80f34c44d1910e09791b47
Hunter99011 <65368551+Hunter99011@users.noreply.github.com>
2024-04-29 10:02:25 +0200

    Add final version

--- gyak10/OVWBWL_2.js
+++ gyak10/OVWBWL_2.js
@@ -1,9 +1,17 @@
-const htmlCodeButton = document.querySelector('#HTMLCodeButton');
-htmlCodeButton.addEventListener("click", () => {
-    alert("<p><b>ME Geik PTI</b></p>");
-});
+$(document).ready(function(){
+    const htmlCodeButton = document.querySelector('#HTMLCodeButton');
+    htmlCodeButton.addEventListener("click", () => {
+        alert("<p><b>ME Geik PTI</b></p>");
+    });
 
-const textButton = document.querySelector('#textButton');
-textButton.addEventListener("click", () => {
-    alert("HTML Kód...");
+    const textButton = document.querySelector('#textButton');
+    textButton.addEventListener("click", () => {
+        alert("HTML Kód...");
+    });
+
+    const valueButton = document.querySelector('#ValueButton');
+    valueButton.addEventListener("click", () => {
+        let nameinputval = $(".nameinput").val();
+        alert(nameinputval);
+    });
 });
